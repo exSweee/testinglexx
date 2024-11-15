06bae1f2813114b64213aa9b044024ba9d7e382b
exSweee <ihostinger2@gmail.com>
2024-11-15 11:14:41 +0700

    Add files via upload

---
+++ scripts/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHqrbHzl9QlyR_oaDJ2t80gdPEJ-uD4bY",
+    authDomain: "lexxhome-cb3b8.firebaseapp.com",
+    databaseURL: "https://lexxhome-cb3b8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "lexxhome-cb3b8",
+    storageBucket: "lexxhome-cb3b8.firebasestorage.app",
+    messagingSenderId: "221283046838",
+    appId: "1:221283046838:web:7bc6c26d4cdd1be2631d17"
+  };
+
+    const app = firebase.initializeApp(firebaseConfig);
